25333f7c4b453e37f075bb0ee1bf1d834cf1fc7d
Anna Rennó <110424006+acrenno@users.noreply.github.com>
2023-02-16 12:52:46 -0300

    Update Main.java

--- AULA1_Introducao_GIT/src/main/java/org/example/Main.java
+++ AULA1_Introducao_GIT/src/main/java/org/example/Main.java
@@ -6,15 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        
+        //sistema de almoxerifado
+        
         System.out.println("BEM VINDO AO ALMOXERIFADO!!");
         System.out.println("RESISTOR | CAPACITOR | PROTOBOARD");
         System.out.println("FACA SUA REQUISICAO:");
 
-        // SISTEMA DE ALMOXERIFADO
 
-        Scanner in = new Scanner(System.in);
-        String componente = in.nextLine();
+        Scanner in = new Scanner(System.in); //classe scanner para entrada de dados
+        String componente = in.nextLine(); //entrando com o componente em questao
 
+        
+        //verificando qual componente eu requisitei e se tem no almoxerifado
         switch (componente) {
             case "RESISTOR":
                 System.out.println("Requisitou um resistor");
