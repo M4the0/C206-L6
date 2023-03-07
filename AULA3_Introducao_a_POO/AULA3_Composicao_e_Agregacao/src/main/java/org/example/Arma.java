24e9713a20bf649fa05d987e09f0584dd2067969
Anna Clara Renno <anna.clara@ges.inatel.br>
2023-03-07 11:55:30 -0300

    Codigo desenvolvido durante a aula sobre introducao a POO

---
+++ AULA3_Introducao_a_POO/AULA3_Composicao_e_Agregacao/src/main/java/org/example/Arma.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Arma {
+    //atributos da nave
+
+    String tipo;
+    int forca;
+
+    //construtor da nave
+    public Arma(String tipo, int forca) {
+        System.out.println("Arma criada!!");
+        this.tipo = tipo;
+        this.forca = forca;
+    }
+}
